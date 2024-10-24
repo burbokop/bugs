28a2e08bb7b1ee69ad6785a3a794fc43750bd8a1
Borys Boiko <burbokop@gmail.com>
2024-10-24 09:52:31 +0200

    Migrated to slint

--- src/main.rs
+++ src/main.rs
@@ -2,117 +2,119 @@
 #![feature(extract_if)]
 
 use complexible::complex_numbers::{Angle, ComplexNumber};
-use draw::draw_filled_triangle;
 use environment::Environment;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
+use sdl2::surface::Surface;
+use slint::{PlatformError, SharedPixelBuffer, Timer, TimerMode};
 use utils::Float;
-use std::time::{Duration, Instant};
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::time::Instant;
 
 mod environment;
 mod brain;
 mod utils;
 mod bug;
-mod draw;
 mod chromo_utils;
 
-fn complex_to_point(complex: ComplexNumber) -> Point {
-    (complex.real() as i32, complex.imag() as i32).into()
+slint::slint! {
+    export { MainWindow } from "src/main.slint";
 }
 
-pub fn main() -> Result<(), String> {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-
+pub fn main() -> Result<(), PlatformError> {
     let width = 800;
     let height = 600;
 
     let mut rng = rand::thread_rng();
 
-    let mut environment = Environment::new(
+    let environment = Rc::new(RefCell::new(Environment::new(
         &mut rng,
         0. ..(width as Float),
         0. ..(height as Float),
         0. ..1.,
         256
-    );
+    )));
 
-    let window = video_subsystem
-        .window("rust-sdl2 demo: Video", width, height)
-        .position_centered()
-        .opengl()
-        .build()
-        .map_err(|e| e.to_string())?;
+    let main_window = MainWindow::new().unwrap();
 
-    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
-
+    let timer = Timer::default();
     let mut last_tick_instant = Instant::now();
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-
-        let now = Instant::now();
-        let dt = now - last_tick_instant;
-        last_tick_instant = now;
-        environment.proceed(dt, &mut rng);
-
-        canvas.set_draw_color(Color::RGB(211, 250, 199));
-        canvas.clear();
-
-        canvas.set_draw_color(Color::RGB(73, 54, 87));
-        for food in environment.food() {
-            canvas.fill_rect(Rect::from_center(
-                (food.x() as i32, food.y() as i32),
-                (food.energy() * 10.) as u32,
-                (food.energy() * 10.) as u32
-            )).unwrap();
-        }
-
-        canvas.set_draw_color(Color::RGB(255, 183, 195));
-        for bug in environment.bugs() {
-            let rotation = ComplexNumber::from_polar(1., Angle::from_radians(bug.rotation()));
-            let pos = ComplexNumber::from_cartesian(bug.x(), bug.y());
-
-            let size = 5.;
-
-            let p0 = ComplexNumber::from_cartesian(4. * size, 0. * size);
-            let p1 = ComplexNumber::from_cartesian(-1. * size, -1. * size);
-            let p2 = ComplexNumber::from_cartesian(-1. * size, 1. * size);
-
-            let pp0 = p0.mul(&rotation).add(&pos);
-            let pp1 = p1.mul(&rotation).add(&pos);
-            let pp2 = p2.mul(&rotation).add(&pos);
-
-            draw_filled_triangle(
-                &mut canvas,
-                complex_to_point(pp0),
-                complex_to_point(pp1),
-                complex_to_point(pp2),
-                Color::RGB(255, 183, 195));
-        }
-
-        canvas.present();
-
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
+    {
+        let environment = environment.clone();
+        timer.start(TimerMode::Repeated, std::time::Duration::from_millis(1000 / 30), move || {
+            let now = Instant::now();
+            let dt = now - last_tick_instant;
+            last_tick_instant = now;
+            environment.borrow_mut().proceed(dt, &mut rng);
+        });
     }
 
-    Ok(())
+    main_window.on_render_environment(move |_: i64| -> slint::Image {
+        let mut pixel_buffer = SharedPixelBuffer::new(width, height);
+        let size = (pixel_buffer.width(), pixel_buffer.height());
+        assert_eq!(pixel_buffer.as_bytes().len(), pixel_buffer.width() as usize * pixel_buffer.height() as usize * 4);
+
+        {
+            let surface = Surface::from_data(
+                pixel_buffer.make_mut_bytes(),
+                size.0,
+                size.1,
+                size.0 * 4,
+                sdl2::pixels::PixelFormatEnum::RGBA32
+            ).unwrap();
+
+            let mut canvas = surface.into_canvas().unwrap();
+
+            canvas.set_draw_color(Color::RGB(211, 250, 199));
+            canvas.clear();
+
+            let environment = environment.borrow();
+
+            canvas.set_draw_color(Color::RGB(73, 54, 87));
+            for food in environment.food() {
+                canvas.fill_rect(Rect::from_center(
+                    (food.x() as i32, food.y() as i32),
+                    (food.energy() * 10.) as u32,
+                    (food.energy() * 10.) as u32
+                )).unwrap();
+            }
+
+            canvas.set_draw_color(Color::RGB(255, 183, 195));
+            for bug in environment.bugs() {
+                let rotation = ComplexNumber::from_polar(1., Angle::from_radians(bug.rotation()));
+                let pos = ComplexNumber::from_cartesian(bug.x(), bug.y());
+
+                let size = 5.;
+
+                let p0 = ComplexNumber::from_cartesian(4. * size, 0. * size);
+                let p1 = ComplexNumber::from_cartesian(-1. * size, -1. * size);
+                let p2 = ComplexNumber::from_cartesian(-1. * size, 1. * size);
+
+                let pp0 = p0.mul(&rotation).add(&pos);
+                let pp1 = p1.mul(&rotation).add(&pos);
+                let pp2 = p2.mul(&rotation).add(&pos);
+
+                canvas
+                    .filled_trigon(
+                        pp0.real() as i16,
+                        pp0.imag() as i16,
+                        pp1.real() as i16,
+                        pp1.imag() as i16,
+                        pp2.real() as i16,
+                        pp2.imag() as i16,
+                        Color::RGB(255, 183, 195),
+                    )
+                    .unwrap();
+            }
+
+            canvas.present();
+        }
+        slint::Image::from_rgba8(pixel_buffer)
+    });
+
+    main_window.run()
 }
 
 
